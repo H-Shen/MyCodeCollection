0cf4e2cabb52078669968b40269c64f6c0478e33
SegmentNeverFault <22427365+H-Shen@users.noreply.github.com>
2025-07-31 05:46:00 -0700

    Update mergeSort_test.go

--- Miscellaneous/GoMergeSort/mergeSort_test.go
+++ Miscellaneous/GoMergeSort/mergeSort_test.go
@@ -1,30 +1,45 @@
 package main
 
 import (
-	"math/rand"
-	"testing"
-	"time"
+    "math/rand"
+    "testing"
+    "time"
 )
 
 const N = 1e6
 
-func getRandomElements() []int {
-	res := make([]int, N)
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < len(res); i++ {
-		res[i] = rand.Int()
-	}
-	return res
+var src []int
+
+func init() {
+    // Seed once, build one big random slice up front
+    rand.Seed(time.Now().UnixNano())
+    src = make([]int, N)
+    for i := range src {
+        src[i] = rand.Int()
+    }
 }
 
-func BenchmarkParallelMergeSort(b *testing.B)  {
-	for i := 0; i < b.N; i++ {
-		parMergesort(getRandomElements())
-	}
+// clone makes a fresh copy so each iteration sorts the same data
+func clone(in []int) []int {
+    out := make([]int, len(in))
+    copy(out, in)
+    return out
 }
 
-func BenchmarkSeqMergeSort(b *testing.B)  {
-	for i := 0; i < b.N; i++ {
-		seqMergesort(getRandomElements())
-	}
+func BenchmarkMergeSorts(b *testing.B) {
+    b.ReportAllocs() // show you how many allocations each sort does
+
+    b.Run("Parallel", func(b *testing.B) {
+        for i := 0; i < b.N; i++ {
+            data := clone(src)
+            parMergesort(data)
+        }
+    })
+
+    b.Run("Sequential", func(b *testing.B) {
+        for i := 0; i < b.N; i++ {
+            data := clone(src)
+            seqMergesort(data)
+        }
+    })
 }
