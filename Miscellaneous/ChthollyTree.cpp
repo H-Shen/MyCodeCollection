b044768821a41ca43abdc335ddeb8104fafbb077
hs <haohu3991@gmail.com>
2025-12-15 03:33:55 -0800

    Update ChthollyTree.cpp

--- Miscellaneous/ChthollyTree.cpp
+++ Miscellaneous/ChthollyTree.cpp
@@ -1,256 +1,239 @@
-#include <bits/extc++.h>
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+#include <set>
+#include <utility>
+#include <vector>
 
-using namespace std;
-using namespace __gnu_pbds;
 using ll = long long;
 
-namespace IO {
-    template<typename T>
-    inline
-    void read(T &t) {
-        int n = 0;
-        int c = getchar_unlocked();
-        t = 0;
-        while (!isdigit(c)) n |= c == '-', c = getchar_unlocked();
-        while (isdigit(c)) t = t * 10 + c - 48, c = getchar_unlocked();
-        if (n) t = -t;
-    }
-
-    template<typename T, typename... Args>
-    inline
-    void read(T &t, Args &... args) {
-        read(t);
-        read(args...);
-    }
-
-    template<typename T>
-    inline void write(T x) {
-        if (x < 0) x = -x, putchar_unlocked('-');
-        if (x > 9) write(x / 10);
-        putchar_unlocked(x % 10 + 48);
-    }
-
-    template<typename T>
-    inline void writeln(T x) {
-        write(x);
-        putchar_unlocked('\n');
-    }
-}
-
-ll mod_pow(ll a, ll p, ll m) {
-    if (m == 1) return 0;
-    ll r;
-    for (r = 1, a %= m; p; a = (a * a) % m, p >>= 1) if (p % 2) r = (r * a) % m;
-    return r % m;
-}
-
-namespace ChthollyTree {
-
-    constexpr ll NOT_FOUND = numeric_limits<ll>::min();
-
-    // Definition of a node in the tree
+class ChthollyTree {
     struct Node {
-        int left;   // each Node in the set A has unique 'left'
-        int right;
-        mutable ll weight;
-
-        Node() = delete;
-
-        explicit Node(int left, int right = 0, ll weight = 0) : left(left),
-                                                                right(right),
-                                                                weight(weight) {}
-
-        bool operator<(const Node &other) const {
-            return left < other.left;
-        }
+        int l;
+        int r;
+        mutable ll v;
+        bool operator<(const Node& o) const { return l < o.l; }
     };
 
-    // Define the tree
-    set<Node> A;
-    // Operations associated with the tree
+    std::set<Node> s_;
+    int n_ = 0;
 
-    // Split: split the interval [l, r] to [l, position-1] and [position, r]
-    // Precondition: the tree is not empty
-    // and returns the iterator points to the second interval
-    set<Node>::iterator split(int position) {
-        // Check the node that its left is the first one such that left >= position
-        auto iter = A.lower_bound(Node(position));
-        // If the iterator does not point to the end of the set and its left == position,
-        // just return it
-        if (iter != A.end() && iter->left == position) {
-            return iter;
-        }
-        --iter;
-        int l = iter->left;
-        int r = iter->right;
-        ll w = iter->weight;
-        A.erase(iter);
-        A.insert(Node(l, position - 1, w));
-        auto temp = A.insert(Node(position, r, w));
-        return temp.first;
+    /*
+     * Split the segment at position pos (0-indexed).
+     * Returns an iterator to the segment whose l == pos.
+     *
+     * Rules:
+     * - If pos <= 0, returns begin().
+     * - If pos >= n_, returns end() (no split).
+     *
+     * Precondition:
+     * - s_ is a full partition of [0..n_-1] with non-overlapping segments.
+     */
+    std::set<Node>::iterator split_(int pos) {
+        if (pos <= 0) return s_.begin();
+        if (pos >= n_) return s_.end();
+
+        auto it = s_.lower_bound(Node{pos, 0, 0LL});
+        if (it != s_.end() && it->l == pos) return it;
+
+        // pos must be inside the previous segment.
+        --it;
+
+        const int l = it->l;
+        const int r = it->r;
+        const ll v = it->v;
+
+        s_.erase(it);
+        s_.insert(Node{l, pos - 1, v});
+        return s_.insert(Node{pos, r, v}).first;
     }
 
-    // Assign: assign the weight of all nodes that cover the interal [l, r] to assigned value
-    void assign(int left, int right, ll new_weight) {
-        auto iter_right = split(right + 1);
-        auto iter_left = split(left);
-        A.erase(iter_left, iter_right);
-        A.insert(Node(left, right, new_weight));
+    /*
+     * Normalize x into [0, mod).
+     */
+    static ll norm_mod_(ll x, ll mod) {
+        x %= mod;
+        if (x < 0) x += mod;
+        return x;
     }
 
-    // Update: update the weight of all nodes that cover the interal [l, r]
-    void update(int left, int right, ll update_weight) {
-        auto iter_right = split(right + 1);
-        auto iter_left = split(left);
-        for (; iter_left != iter_right; ++iter_left) {
-            iter_left->weight += update_weight;
-        }
-    }
+    /*
+     * Portable overflow-safe (no __int128) multiplication: (a * b) % mod.
+     * Uses binary multiplication with safe doubling under modulo.
+     *
+     * Requirement: mod > 0.
+     */
+    static ll mulmod_(ll a, ll b, ll mod) {
+        a = norm_mod_(a, mod);
+        b = norm_mod_(b, mod);
 
-    // Sum: Range-sum the weight of all nodes that cover the interal [l, r] and mod m
-    ll sum(int left, int right, ll mod) {
-        ll result = 0;
-        auto iter_right = split(right + 1);
-        auto iter_left = split(left);
-        ll freq;
-        for (; iter_left != iter_right; ++iter_left) {
-            freq = iter_left->right - iter_left->left + 1;
-            result = (result + freq * iter_left->weight) % mod;
-        }
-        return result;
-    }
-
-    // Sum: Range-sum the weight of all nodes that cover the interal [l, r]
-    ll sum(int left, int right) {
-        ll result = 0;
-        auto iter_right = split(right + 1);
-        auto iter_left = split(left);
-        ll freq;
-        for (; iter_left != iter_right; ++iter_left) {
-            freq = iter_left->right - iter_left->left + 1;
-            result += freq * iter_left->weight;
-        }
-        return result;
-    }
-
-    ll pow_sum(int left, int right, ll pow, ll mod) {
-        ll result = 0;
-        auto iter_right = split(right + 1);
-        auto iter_left = split(left);
-        ll freq;
-        for (; iter_left != iter_right; ++iter_left) {
-            freq = iter_left->right - iter_left->left + 1;
-            result = (result + freq * mod_pow(iter_left->weight, pow, mod)) %
-                     mod;
-        }
-        return result;
-    }
-
-    // Obtain the nth number in the interval [l, r] in non-decreasing order.
-    // If non_decreasing is false, return the kth number in non-increasing order.
-    // return NOT_FOUND if such nth number does not exist
-    ll nth(int left, int right, int n, bool non_decreasing = true) {
-        if (!non_decreasing) {
-            n = right - left + 2 - n;
-        }
-        vector<pair<ll, int> > temp_vec;
-        auto iter_right = split(right + 1);
-        auto iter_left = split(left);
-        int length;
-        for (; iter_left != iter_right; ++iter_left) {
-            length = iter_left->right - iter_left->left + 1;
-            temp_vec.emplace_back(make_pair(iter_left->weight, length));
-        }
-        sort(temp_vec.begin(), temp_vec.end(),
-             [](const pair<ll, int> &lhs, const pair<ll, int> &rhs) {
-                 if (lhs.first == rhs.first) {
-                     return (lhs.second < rhs.second);
-                 }
-                 return (lhs.first < rhs.first);
-             });
-        for (const auto &i : temp_vec) {
-            n -= i.second;
-            if (n <= 0) {
-                return i.first;
+        ll res = 0;
+        while (b > 0) {
+            if (b & 1LL) {
+                res += a;
+                if (res >= mod) res -= mod;
             }
+            // a = (2*a) % mod without signed overflow
+            if (a >= mod - a) a = a - (mod - a);
+            else a = a + a;
+            b >>= 1LL;
         }
-        return NOT_FOUND;
+        return res;
     }
 
-    // Check if the interval [left, right] has unique weights
-    bool unique_check(int left, int right) {
-        auto iter_right = split(right + 1);
-        auto iter_left = split(left);
-        unordered_set<ll> temp_unset;
-        for (; iter_left != iter_right; ++iter_left) {
-            temp_unset.insert(iter_left->weight);
-            if (temp_unset.size() > 1) {
-                return false;
-            }
+    /*
+     * Fast exponentiation modulo mod using mulmod_().
+     *
+     * Requirement: mod > 0, exp >= 0.
+     */
+    static ll qpow_(ll base, ll exp, ll mod) {
+        ll res = 1 % mod;
+        base = norm_mod_(base, mod);
+        while (exp > 0) {
+            if (exp & 1LL) res = mulmod_(res, base, mod);
+            base = mulmod_(base, base, mod);
+            exp >>= 1LL;
         }
-        return true;
+        return res;
     }
-}
 
-ll n, m, seed, v_max, l, r, x, op, y;
+public:
+    ChthollyTree() = default;
 
-ll rnd() {
-    ll ret = seed;
-    seed = (seed * 7 + 13) % 1000000007;
+    /*
+     * Build from a 0-indexed array a[0..n-1].
+     * Each position becomes a singleton segment [i, i].
+     */
+    void build(const std::vector<ll>& a) {
+        n_ = static_cast<int>(a.size());
+        s_.clear();
+        for (int i = 0; i < n_; ++i) {
+            s_.insert(Node{i, i, a[static_cast<size_t>(i)]});
+        }
+    }
+
+    /*
+     * Add x to all values in [l, r] (0-indexed, inclusive).
+     */
+    void add(int l, int r, ll x) {
+        const auto itr = split_(r + 1);
+        const auto itl = split_(l);
+        for (auto it = itl; it != itr; ++it) it->v += x;
+    }
+
+    /*
+     * Assign x to all values in [l, r] (0-indexed, inclusive).
+     */
+    void assign(int l, int r, ll x) {
+        auto itr = split_(r + 1);
+        auto itl = split_(l);
+        s_.erase(itl, itr);
+        s_.insert(Node{l, r, x});
+    }
+
+    /*
+     * Return the k-th smallest value in [l, r] (k is 1-based).
+     * Returns -1 if k is invalid.
+     */
+    ll kth(int l, int r, int k) {
+        const auto itr = split_(r + 1);
+        const auto itl = split_(l);
+
+        std::vector<std::pair<ll, int>> vec;
+        vec.reserve(64);
+
+        for (auto it = itl; it != itr; ++it) {
+            vec.emplace_back(it->v, it->r - it->l + 1);
+        }
+
+        std::ranges::sort(vec);  // sort by (value, count)
+
+        for (const auto& [val, cnt] : vec) {
+            k -= cnt;
+            if (k <= 0) return val;
+        }
+        return -1LL;
+    }
+
+    /*
+     * Compute sum_{i=l..r} (a[i]^x) mod y.
+     * Requirement: y > 0, x >= 0.
+     */
+    ll powsum(int l, int r, ll x, ll y) {
+        const auto itr = split_(r + 1);
+        const auto itl = split_(l);
+
+        ll ans = 0;
+        for (auto it = itl; it != itr; ++it) {
+            const ll len = static_cast<ll>(it->r - it->l + 1) % y;
+            const ll pw = qpow_(it->v, x, y);
+            ans = (ans + mulmod_(len, pw, y)) % y;
+        }
+        return ans;
+    }
+};
+
+/*
+ * Deterministic PRNG used by the classic ODT demo.
+ */
+static ll g_seed = 0;
+
+static ll rnd() {
+    const ll ret = g_seed;
+    g_seed = (g_seed * 7 + 13) % 1000000007LL;
     return ret;
 }
 
-// An example of ChthollyTree:
-// given an array L of n elements, a Chtholly Tree supports 4 operations:
-// 1 l r x:
-// For each $i$ such that $l \leq i \leq r$, update L[i] by $L[i] = L[i] + x$
-// 2 l r x:
-// For each $i$ such that $l \leq i \leq r$, assign $L[i] = x$
-// 3 l r x:
-// Print the kth-smallest number in from L[l] to L[r], duplicated values are also considered
-// 4 l r x y
-// Print $(\sum\limits_{i=l}^{r} L_{i}^{x}) \mod y$
-//
-// If the data sample and the operations are strictly randomly generated,
-// the time complexity is $\mathcal{O}(n \log( \log(n)))$ when using std::set
-//
-
 int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-    IO::read(n, m, seed, v_max);
-    // init
-    for (int i = 1; i <= n; ++i) {
-        ChthollyTree::A.insert(ChthollyTree::Node(i, i, (rnd() % v_max) + 1));
+    int n = 0;
+    int m = 0;
+    ll seed = 0;
+    ll v_max = 0;
+
+    // Demo input: n m seed v_max
+    if (!(std::cin >> n >> m >> seed >> v_max)) return 0;
+    g_seed = seed;
+
+    // 0-indexed initial array: a[0..n-1]
+    std::vector<ll> a(static_cast<size_t>(n));
+    for (int i = 0; i < n; ++i) {
+        a[static_cast<size_t>(i)] = (rnd() % v_max) + 1;
     }
-    for (int i = 1; i <= m; ++i) {
-        op = (rnd() % 4) + 1;
-        l = (rnd() % n) + 1;
-        r = (rnd() % n) + 1;
-        if (l > r) {
-            swap(l, r);
-        }
-        if (op == 3) {
-            x = (rnd() % (r - l + 1)) + 1;
-        } else {
-            x = (rnd() % v_max) + 1;
-        }
-        if (op == 4) {
-            y = (rnd() % v_max) + 1;
-        }
-        // process
-        switch (op) {
-            case 1:
-                ChthollyTree::update(l, r, x);
-                break;
-            case 2:
-                ChthollyTree::assign(l, r, x);
-                break;
-            case 3:
-                IO::writeln(ChthollyTree::nth(l, r, x));
-                break;
-            case 4:
-                IO::writeln(ChthollyTree::pow_sum(l, r, x, y));
-                break;
+
+    ChthollyTree tree;
+    tree.build(a);
+
+    // Random operations on 0-indexed ranges [l, r]:
+    // 1 l r x : add x
+    // 2 l r x : assign x
+    // 3 l r k : kth smallest
+    // 4 l r x y : sum(a[i]^x) mod y
+    for (int i = 0; i < m; ++i) {
+        const ll op = (rnd() % 4) + 1;
+
+        int l = static_cast<int>(rnd() % n);
+        int r = static_cast<int>(rnd() % n);
+        if (l > r) std::swap(l, r);
+
+        ll x = 0;
+        ll y = 0;
+
+        if (op == 3) x = (rnd() % (r - l + 1)) + 1;  // k in [1..len]
+        else x = (rnd() % v_max) + 1;
+
+        if (op == 4) y = (rnd() % v_max) + 1;
+
+        if (op == 1) {
+            tree.add(l, r, x);
+        } else if (op == 2) {
+            tree.assign(l, r, x);
+        } else if (op == 3) {
+            std::cout << tree.kth(l, r, static_cast<int>(x)) << '\n';
+        } else {  // op == 4
+            std::cout << tree.powsum(l, r, x, y) << '\n';
         }
     }
 
